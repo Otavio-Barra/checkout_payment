466943339c5f1dae16747a42a8ca54f647423388
Otavio-Barra <otaviombarra@gmail.com>
2024-05-06 21:50:45 -0300

    feito mudancas texto

--- src/imgs/curso-mp/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
- return [
-    'accesstoken' => '------------------',
-    'url_notification_sdk' => 'https://95d60c603db969.lhr.life/curso/sdk/notification.php',
-    'url_notification_api' => 'https://95d60c603db969.lhr.life/curso/api/notification.php'
-];
